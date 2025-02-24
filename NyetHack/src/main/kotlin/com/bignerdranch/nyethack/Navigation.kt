488b6bb5c15ab40d816a868bbfe95df0cd56ec5f
Tennant S <emptyhuskgaming@gmail.com>
2025-02-23 20:23:44 -0500

    Changed north and south coordinate

--- NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
@@ -7,9 +7,9 @@ data class Coordinate(val x: Int, val y: Int) {
 enum class Direction(
     private val directionCoordinate: Coordinate
 ) {
-    North(Coordinate(0, -1)),
+    North(Coordinate(0, 1)),
     East(Coordinate(1, 0)),
-    South(Coordinate(0, 1)),
+    South(Coordinate(0, -1)),
     West(Coordinate(-1, 0));
 
     fun updateCoordinate(coordinate: Coordinate) = coordinate + directionCoordinate
